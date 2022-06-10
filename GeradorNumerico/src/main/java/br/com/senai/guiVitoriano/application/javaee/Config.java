a8378440d8fe0c3648a34853e3b9f23a69f4472d
Fernanda <Fernanda@DESKTOP-369LQ9G>
2022-06-10 12:55:05 -0300

    COMENTÁRIOS OK

---
+++ GeradorNumerico/src/main/java/br/com/senai/guiVitoriano/application/javaee/Config.java
@@ -0,0 +1,11 @@
+package br.com.senai.guiVitoriano.application.javaee;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+import javax.faces.annotation.FacesConfig.Version;
+
+@FacesConfig(version = Version.JSF_2_3) //Diz ao Eclipse qual versão ele deve utilizar 
+@ApplicationScoped //Dura apenas durante o funcionamento da aplicação
+public class Config {
+	
+}
