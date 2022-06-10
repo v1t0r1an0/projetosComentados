01ac528b0f50ccb6a34e077cdf3b40d703118646
Fernanda <Fernanda@DESKTOP-369LQ9G>
2022-06-10 15:30:57 -0300

    COMENTÁRIOS OK

---
+++ TemplateCabecalho/src/main/java/br/com/senai/guiVitoriano/application/model/Despesas.java
@@ -0,0 +1,67 @@
+package br.com.senai.guiVitoriano.application.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+public class Despesas {
+	
+	@Id //Determina que o id vai ser a chave primária
+	@GeneratedValue //Determina que a respondabilidade da geração dos ids é do provedor de persistência
+	private Integer id;
+	private String data;
+	private String descricao;
+	private String valor;
+	private Boolean edit;
+	
+	public String getData() {
+		return data;
+	}
+	
+	
+	public Despesas(String data, String descricao, String valor) {
+		super();
+		this.data = data;
+		this.descricao = descricao;
+		this.valor = valor;
+	}
+	
+	public Despesas() {
+		this.data = data;
+		this.descricao = descricao;
+		this.valor = valor;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getValor() {
+		return valor;
+	}
+	public void setValor(String valor) {
+		this.valor = valor;
+	}
+	public Boolean getEdit() {
+		return edit;
+	}
+	public void setEdit(Boolean edit) {
+		this.edit = edit;
+}
+
+
+	
+}
